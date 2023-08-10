d208a34a487c050ba669e6e70790fef7b438634a
Hisgrace1998 <112432622+Hisgrace1998@users.noreply.github.com>
2023-08-10 12:19:25 +0300

    Add files via upload

---
+++ choose your own adventure.py
@@ -0,0 +1,36 @@
+
+name = input("Type your name:")
+print("Welcome", name, "to this adventure!")
+
+answer =
+ iput("You are on dirt road,it has come to an endand you can go left or right?")
+
+if answer =="left":
+answer = input("You come to a river,you can walk around it or swim accross")
+
+  if answer =="swim"
+     print("You swam across and were eaten by an alligator")
+
+   elif answer == "walk"
+    print("You walked for many miles,rane out of water and lost the game")
+
+
+
+  else:
+      print("Not a valid option. You lose.")
+
+elif answer =="right":
+     answer = input("You come to a bridge,it looks wobbly,do you want to cross")
+
+     if answer == "back":
+
+    print("You go back and lose.")
+lif answer== "cross":
+    answer= input("You cross the bridge and meet a stranger. Do you talk to them")
+
+    if answer == "yes":
+    
+    elif answer =="no"
+      print("You ignore the stranger and they are offended to loose")
+else:
+    print('Not a valid option you loose.')
